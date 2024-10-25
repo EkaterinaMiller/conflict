b21448ca3f5892b459252740d9ab57a42e855730
Ekaterina Miller <evmiller@nic.edu>
2024-10-25 09:29:59 -0700

    fix conflict

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     cout<< "Greeting...\n";
-=======
     cout<< "Hello World\n";
->>>>>>> 18e33e01369e68fc011cee266b4004b22864f7b7
     return 0;
 }
